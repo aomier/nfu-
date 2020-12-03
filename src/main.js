0185c4bde473d75e9442c379c31c651bb55fbdec
penggang <2845486124@qq.com>
2020-12-03 21:52:42 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+// 配置默认路径
+axios.defaults.baseUrl = 'http://127.0.0.1:8888/api'
+Vue.prototype.$http = axios
+
+// echarts 已在 public/index.html 中通过 script引入
+Vue.prototype.$echarts = window.echarts
 
 Vue.config.productionTip = false
 
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  devServer: {
+    port: 8999, //端口号
+    open: true, //自动打开浏览器
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: "@/components",
+        content: "@/components/content",
+        common: "@/components/common",
+        assets: "@/assets",
+        network: "@/network",
+        views: "@/views",
+      }
+    }
+  },
+}
