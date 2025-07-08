474803691112385808f86930fe7801d2c74437ca
黄镇浩 <aomier2022@163.com>
2025-07-09 06:40:00 +0800

    Update AnalysisResults.vue

--- src/views/AnalysisResults.vue
+++ src/views/AnalysisResults.vue
@@ -1865,7 +1865,7 @@ export default {
   font-weight: bold;
   text-shadow: 0 0 8px #00bfff, 0 0 2px #fff;
 }
-
+<style lang="less" scoped>
 .table-content, .screen-body, .ai-card-content {
   overflow-y: auto;
   scrollbar-width: thin;
